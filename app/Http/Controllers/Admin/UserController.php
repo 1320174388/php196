cdbe4dd8e134a2fe91dd604200cfc4867653aa7f
DLwuming <1125113858@qq.com>
2018-01-11 10:16:58 +0800

    资源路由

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,83 +3,115 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+
+
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+    /*
+     * 返回用户添加页面
+     */
+    public function add()
+    {
+        return view('user.add');
+    }
+
+    /*
+     * 将用户提交的添加用户数据保存到数据库的user表中，添加用户
+     */
+
+    public function store(Request $request)
+    {
+
+       return  $request->path();
+       return  $request->method();
+//        1. 获取表单提交的数据
+        $input = $request->except('_token');
+//        dd($input);
+
+//        2. 执行添加操作
+        // 插入
+
+        $res = DB::table('user')->insert(
+            ['user_name' => $input['user_name'], 'user_pass' => $input['user_pass']]
+        );
+
+
+
+//        3. 进行添加是否成功的判断，如果成功，跳转到列表页，如果失败，返回添加页
+
+        if($res){
+//            return view('user.index')->with('msg','添加成功');
+             return redirect('user/index')->with('msg','添加成功');
+        }else{
+            return back()->with('msg','添加失败');
+        }
+    }
+
+
+    /*
+     * 返回列表页
      */
     public function index()
     {
-        //
-    }
+        //1 获取所有的列表用户数据
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+//        查询user表
+        $users = DB::table('user')->get();
+//        2. 将数据绑定到列表页上
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        return view('user.list',['users'=>$users]);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+    
+    /*
+     * 返回用户修改页面
      */
-    public function show($id)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        //
+        //根据id找到要修改的用户
+        $user = DB::table('user')->where('id',$id)->first();
+        return view('user.edit',['user'=>$user]);
+        
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+    /*
+     * 提交用户的修改操作
      */
-    public function update(Request $request, $id)
+
+    public function update(Request $request)
     {
-        //
+//        1. 接收用户提交的修改数据 和要修改的用户的id
+            $input = $request->except('_token');
+
+
+//        2. 修改指定用户的数据
+        $res = DB::table('user')
+            ->where('id', $input['id'])
+            ->update(['user_name' => $input['user_name']]);
+
+
+        if($res){
+//            return view('user.index')->with('msg','添加成功');
+            return redirect('user/index')->with('msg','修改成功');
+        }else{
+            return back()->with('msg','修改失败');
+        }
+    }
+    
+    //删除用户
+    public function delete($id)
+    {
+        //根据ID找到要删除的用户记录
+        $res = DB::table('user')->where('id',$id)->delete();
+
+        if($res){
+            return '删除成功';
+        }else{
+            return '删除失败';
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="{{ url('user/update') }}" method="post">
+    <table>
+        <tr>
+            <input type="hidden" name="id" value="{{ $user->id }}">
+
+            <td>用户名</td>
+            {{--生成一个token--}}
+            {{ csrf_field() }}
+            {{--等价于上面的语句--}}
+            {{--<input type="hidden" name="_token" value="{{ csrf_token() }}">--}}
+
+            <td><input type="text" name="user_name" value="{{ $user->user_name }}"></td>
+        </tr>
+
+        <tr>
+            <td><input type="submit" value="提交"></td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
